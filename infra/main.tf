251e8d910d2d0e26e2243297fba49a00f37a1de8
JuusoVe <juuso.vesanto@gmail.com>
2022-10-23 14:31:37 +0300

    trigger

--- infra/main.tf
+++ infra/main.tf
@@ -42,39 +42,39 @@ resource "aws_s3_bucket_acl" "bucket_acl" {
 }
 
 // LAMBDAS TO STORE
-data "archive_file" "lambda_hello_world" {
+data "archive_file" "lambda_main_api" {
   type = "zip"
 
   source_dir  = "${path.module}/dist"
   output_path = "${path.module}/dist.zip"
 }
 
-resource "aws_s3_object" "lambda_hello_world" {
+resource "aws_s3_object" "lambda_main_api" {
   bucket = aws_s3_bucket.lambda_bucket.id
 
   key    = "dist.zip"
-  source = data.archive_file.lambda_hello_world.output_path
+  source = data.archive_file.lambda_main_api.output_path
 
-  etag = filemd5(data.archive_file.lambda_hello_world.output_path)
+  etag = filemd5(data.archive_file.lambda_main_api.output_path)
 }
 
 // THE ACTUAL FUNCTION
-resource "aws_lambda_function" "hello_world" {
-  function_name = "HelloWorld"
+resource "aws_lambda_function" "main_api" {
+  function_name = "MainAPI"
 
   s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_object.lambda_hello_world.key
+  s3_key    = aws_s3_object.lambda_main_api.key
 
   runtime = "nodejs16.x"
-  handler = "hello.handler"
+  handler = "index.lambdaHandler"
 
-  source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
+  source_code_hash = data.archive_file.lambda_main_api.output_base64sha256
 
   role = aws_iam_role.lambda_exec.arn
 }
 
-resource "aws_cloudwatch_log_group" "hello_world" {
-  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
+resource "aws_cloudwatch_log_group" "main_api" {
+  name = "/aws/lambda/${aws_lambda_function.main_api.function_name}"
 
   retention_in_days = 30
 }
@@ -133,19 +133,19 @@ resource "aws_apigatewayv2_stage" "lambda" {
   }
 }
 
-resource "aws_apigatewayv2_integration" "hello_world" {
+resource "aws_apigatewayv2_integration" "main_api" {
   api_id = aws_apigatewayv2_api.lambda.id
 
-  integration_uri    = aws_lambda_function.hello_world.invoke_arn
+  integration_uri    = aws_lambda_function.main_api.invoke_arn
   integration_type   = "AWS_PROXY"
   integration_method = "POST"
 }
 
-resource "aws_apigatewayv2_route" "hello_world" {
+resource "aws_apigatewayv2_route" "main_api" {
   api_id = aws_apigatewayv2_api.lambda.id
 
-  route_key = "GET /hello"
-  target    = "integrations/${aws_apigatewayv2_integration.hello_world.id}"
+  route_key = "GET /health"
+  target    = "integrations/${aws_apigatewayv2_integration.main_api.id}"
 }
 
 resource "aws_cloudwatch_log_group" "api_gw" {
@@ -157,7 +157,7 @@ resource "aws_cloudwatch_log_group" "api_gw" {
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello_world.function_name
+  function_name = aws_lambda_function.main_api.function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
